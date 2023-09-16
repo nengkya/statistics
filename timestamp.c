61448591c41886ed7d7ca9826ff7c60ef5824fe6
nengkya <nengkya@yahoo.com>
2023-09-16 21:26:32 +0700

    mv README.md readme.md

--- timestamp.c
+++ timestamp.c
@@ -5,6 +5,11 @@ int main() {
     /* static define variable static out of scope */
     static char time_buffer[time_size];
     /* const cant be modified the value, but can change where is point to */
+    /*
+    member of struct tm or time {
+	tm_second .. tm_year
+    }
+    */
     const struct tm * time;
     time_t raw_time;
 
