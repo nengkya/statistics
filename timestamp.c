55f7a6dc1e381fc25573370c3fb2470166f2cd21
nengkya <nengkya@yahoo.com>
2023-09-16 20:47:51 +0700

    /* const cant be modified the value, but can change where is point to */

--- timestamp.c
+++ timestamp.c
@@ -4,7 +4,7 @@ int main() {
     #define time_size 40
     /* static define variable static out of scope */
     static char time_buffer[time_size];
-    /* const cant be modified the value, but can change where is point */
+    /* const cant be modified the value, but can change where is point to */
     const struct tm * time;
     time_t raw_time;
 
